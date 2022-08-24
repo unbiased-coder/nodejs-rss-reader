d43ecfacd9cc32dd2614183528a0d2043e3ae909
Unbiased Coder <info@unbiased-coder.com>
2022-08-24 14:29:08 +0300

    adding get rss links and titles

--- nodejs-rss-reader.ts
+++ nodejs-rss-reader.ts
@@ -1,16 +1,53 @@
 import Parser from 'rss-parser';
 
-type CustomFeed = {foo: string};
-type CustomItem = {bar: number};
-
 const parser = new Parser();
 const yahooFinanceFeed = 'https://finance.yahoo.com/news/rssindex';
 
-(async () => {
-  const feed = await parser.parseURL(yahooFinanceFeed);
-  console.log(feed);
+async function readRssFeed(): Promise <any> {
+  return await parser.parseURL(yahooFinanceFeed);
+}
 
-  feed.items.forEach(item => {
-    console.log(item);
+async function getRssLinks(): Promise <string[]> {
+  let feed: any;
+  let feed_links: string[] = [];
+
+  feed = await readRssFeed();
+  if (!feed){
+    console.log('Feed does not contain any data');
+    return [];
+  }
+
+  feed.items.forEach((item: any) => {
+    feed_links.push(item.link);
   });
+  
+  return feed_links;
+}
+
+async function getRssTitles(): Promise <string[]> {
+  let feed: any;
+  let feed_titles: string[] = [];
+
+  feed = await readRssFeed();
+  if (!feed){
+    console.log('Feed does not contain any data');
+    return [];
+  }
+
+  feed.items.forEach((item: any) => {
+    feed_titles.push(item.title);
+  });
+  
+  return feed_titles;
+}
+
+(async () => {
+  let feed_titles: string[];
+  let feed_links: string[];
+
+  feed_titles = await getRssTitles();
+  console.log(feed_titles);
+
+  feed_links = await getRssLinks();
+  console.log(feed_links);
 })();
